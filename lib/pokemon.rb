da1d572762880479ff03fd230d474ade59ebd85a
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-10 04:47:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,7 @@ class Pokemon
     INSERT INTO pokemon(name, type)
     VALUES (?, ?)
     SQL
-    @db.execute(sql, name, type)
+    @db.execute(sql, name, type, id)
   end
   
   def self.find(id, db)
